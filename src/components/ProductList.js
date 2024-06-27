be7e5b8c17cb7aa448467e4ef1821f9f09c231d3
Rafa <rafasilvasimoes25@gmail.com>
2024-06-27 17:31:36 -0300

    [UPD- adicionando produtos no carrinho]

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { useProducts } from '../hooks/useProducts';
+import { useProducts } from '../hooks/useProducts'; 
+import useCart from '../hooks/useCart'; 
 
 const ProductList = () => {
   const { data, error, isLoading } = useProducts(1, 10, 'nome', 'ASC');
+  const { addToCart } = useCart();
 
   if (isLoading) {
     return (
@@ -22,6 +24,7 @@ const ProductList = () => {
         <div key={product.id}>
           <h2>{product.nome}</h2>
           <p>Preço: {product.preco}</p>
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
